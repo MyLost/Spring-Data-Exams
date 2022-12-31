82af44346863f2121881b3067b60331aafecc0ef
darthReader <ddimitrov10@gmail.com>
2022-12-31 11:08:11 +0200

    Exam prep

---
+++ Java DB Spring Data Regular Exam - 3 December 2022/src/main/java/softuni/exam/util/Messages.java
@@ -0,0 +1,17 @@
+package softuni.exam.util;
+
+public class Messages {
+
+    public static String INVALID = "Invalid ";
+    public static String SUCCESSFULLY = "Successfully imported ";
+    public static String INTERVAL = " ";
+
+    public static String PART = "part";
+    public static String MECHANIC = "mechanic";
+    public static String CAR = "car";
+    public static String TASK = "task";
+
+    public static String DASH = " - ";
+    public static String FORMAT_DOUBLE = "%.2f";
+
+}
