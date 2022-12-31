5f89659890fdb4a68f512d1993683d99e1d14e1c
darthReader <ddimitrov10@gmail.com>
2022-12-31 11:06:45 +0200

    Exam prep

---
+++ Java DB Spring Data Regular Exam - 3 December 2022/src/main/java/softuni/exam/models/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package softuni.exam.models.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
